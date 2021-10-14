cc653c308d182a0db21b8cb6f580b86c975c52b9
Artur Santiago <artur.santiago@squadra.com.br>
2021-10-13 23:58:41 -0300

    feat: add new icons for flags and avatar

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,4 @@
 export { default as logo } from './logo.svg';
 export { default as logoSmall } from './logo--small.svg';
+export { default as avatar } from './avatar.svg';
+export { default as avatar2 } from './avatar2.svg';
